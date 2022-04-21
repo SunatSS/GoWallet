1d4b7f274cc3bac6d33c5e38f278d62de4ae2ee3
SYSTEM <sunat.saidov04@mail.ru>
2022-04-21 10:44:02 +0500

    test files rename

--- test/test.sql
+++
@@ -1,9 +0,0 @@
--- password = 12345678
-INSERT INTO accounts (balance, identified, name, phone, password) VALUES 
-(0, FALSE, '0', '0', '$2a$10$W1uTjnpz.h/hbfWuRhO04ekfs6FffeMsIbtFpxLiFhE6eMgW7oMUi'),
-(1000000, FALSE, '1', '1', '$2a$10$W1uTjnpz.h/hbfWuRhO04ekfs6FffeMsIbtFpxLiFhE6eMgW7oMUi'),
-(10000000, TRUE, '2', '2', '$2a$10$W1uTjnpz.h/hbfWuRhO04ekfs6FffeMsIbtFpxLiFhE6eMgW7oMUi');
-
-INSERT INTO transactions (acc_id, amount) VALUES 
-(1, 1000000),
-(2, 10000000);
